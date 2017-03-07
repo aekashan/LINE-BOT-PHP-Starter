686ed66982755782703cb063c46b9731d74d38d2
aekashan <aekashan@gmail.com>
2017-03-08 00:39:41 +0700

    Update verify.php
    test error

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$proxy = 'xxxx:80';
+$proxy = 'yugisang.kaikongtook.com:21';
 $proxyauth = 'gusavoice@gmail.com:system1508';
 $access_token = 'FqJ3FN92d9X6PbAM/pjc/j62/WbI3/wThTJsRNrou5EaecTUPLk2NFypo53q1Okpxpo86hyu9kHeGS63YR2m93TbYZX4PTTkkz2KC/L5hFZ4CjdTfJ+czn9vYT99TYbKiLPaIh4wXEYDLcNR9bChFQdB04t89/1O/w1cDnyilFU=';
 
