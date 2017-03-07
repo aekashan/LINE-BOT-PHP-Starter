038e60f5e69bf22ba1058c4802be5eab4b5073b7
aekashan <aekashan@gmail.com>
2017-03-08 00:35:31 +0700

    Update verify.php
    test error whitelist

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$proxy = 'http://fixie:AHlPTYMdvzgxYpY@velodrome.usefixie.com:80';
+$proxy = 'xxxx:80';
 $proxyauth = 'gusavoice@gmail.com:system1508';
 $access_token = 'FqJ3FN92d9X6PbAM/pjc/j62/WbI3/wThTJsRNrou5EaecTUPLk2NFypo53q1Okpxpo86hyu9kHeGS63YR2m93TbYZX4PTTkkz2KC/L5hFZ4CjdTfJ+czn9vYT99TYbKiLPaIh4wXEYDLcNR9bChFQdB04t89/1O/w1cDnyilFU=';
 
